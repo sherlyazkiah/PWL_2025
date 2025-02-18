22e5d3e65f6bdb7384f7a81fdfcfa49d13b55e93
sherlyazkiah <sherlyazkiah12@gmail.com>
2025-02-18 12:53:11 +0700

    basic routing

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Basic routing
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return '2341720241 Sherly Lutfi Azkiah Sulistyawati';
+});
