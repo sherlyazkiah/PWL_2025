44d2b633c14bdd77f6f946433331817bf04fc7c6
sherlyazkiah <sherlyazkiah12@gmail.com>
2025-02-22 15:00:55 +0700

    resource controller

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\PhotoController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -64,3 +65,9 @@ Route::get('/hello', [WelcomeController::class,'hello']);
 Route::get('/', HomeController::class);
 Route::get('/about', AboutController::class);
 Route::get('/articles/{id}', ArticleController::class);
+
+//Resource Controller
+//Route::resource('photos', PhotoController::class);
+//Limiting the routes
+Route::resource('photos', PhotoController::class)->only(['index', 'show']);
+Route::resource('photos', PhotoController::class)->except(['create', 'store', 'update', 'destroy']);
